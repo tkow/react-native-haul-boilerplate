0914dcf7380d5f6b4ab20743644fe9e5ab92fd76
tkow <t.kusama0613@outlook.jp>
2019-09-01 22:02:27 +0900

    haul upgrade

--- haul.config.js
+++ haul.config.js
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { makeConfig, withPolyfills } from '@haul-bundler/preset-0.60';
-// import { smart } from 'webpack-merge'
+import { smart } from 'webpack-merge'
 
 var detoxFlg = process.env.APP_ENV === 'detox_tests';
 var detoxExtension = detoxFlg ? ['.mock.behaviour.js'] : [];
@@ -8,24 +8,12 @@ var detoxExtension = detoxFlg ? ['.mock.behaviour.js'] : [];
 const webpackConfig = ({ config, env }) => {
   const { platform } = env;
   const addConfig = {
-    resolveLoader: {
-      modules: ['node_modules', path.resolve(__dirname, 'loaders')]
-    },
     module: {
       rules: [
         {
           test: /\.jsx?$/,
           include: [
             path.join(__dirname, 'app/'),
-            path.join(__dirname, 'native-base-theme'),
-            path.join(__dirname, 'node_modules/native-base'),
-            path.join(__dirname, 'vender/ts_modules'),
-            path.join(__dirname, 'node_modules/native-base-shoutem-theme'),
-            path.join(__dirname, 'node_modules/rn-fetch-blob'),
-            path.join(
-              __dirname,
-              'node_modules/@react-native-community/async-storage'
-            )
           ],
           use: {
             loader: 'babel-loader',
@@ -51,29 +39,14 @@ const webpackConfig = ({ config, env }) => {
               loader: 'ts-loader'
             }
           ]
-        },
-        {
-          test: /\.yaml|\.yml$/,
-          include: [path.join(__dirname, 'app/')],
-          use: [
-            {
-              loader: 'yaml-json-loader'
-            }
-          ]
         }
       ]
     },
     resolve: {
       alias: {
-        '~': path.join(__dirname, 'app/'),
-        'lev-ts-lib': path.join(__dirname, 'vender', 'ts_modules/'),
-        'native-base-theme': path.join(__dirname, 'native-base-theme/'),
-        'react-native-image-picker': path.join(
-          __dirname,
-          'node_modules/react-native-image-picker/lib/commonjs/index.js'
-        )
+        '~': path.join(__dirname, 'src/')
       },
-      modules: [path.resolve(__dirname, 'app'), 'node_modules'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
       extensions: [
         `.${platform}.tsx`,
         `.${platform}.ts`,
@@ -93,19 +66,19 @@ const webpackConfig = ({ config, env }) => {
       ]
     }
   };
-  // smart(base,addConfig); refの問題が解決するまで保留
-  config.resolve.extensions = config.resolve.extensions.concat(
-    addConfig.resolve.extensions
-  );
-  config.resolve.alias = config.resolve.alias || {};
-  Object.keys(addConfig.resolve.alias).forEach(key => {
-    config.resolve.alias[key] = addConfig.resolve.alias[key];
-  });
-  config.resolve.modules = config.resolve.modules
-    ? config.resolve.modules.concat(addConfig.resolve.modules)
-    : addConfig.resolve.modules;
-  config.module.rules = config.module.rules.concat(addConfig.module.rules);
-  config.resolveLoader = addConfig.resolveLoader;
+  smart(base,addConfig); refの問題が解決するまで保留
+  // config.resolve.extensions = config.resolve.extensions.concat(
+  //   addConfig.resolve.extensions
+  // );
+  // config.resolve.alias = config.resolve.alias || {};
+  // Object.keys(addConfig.resolve.alias).forEach(key => {
+  //   config.resolve.alias[key] = addConfig.resolve.alias[key];
+  // });
+  // config.resolve.modules = config.resolve.modules
+  //   ? config.resolve.modules.concat(addConfig.resolve.modules)
+  //   : addConfig.resolve.modules;
+  // config.module.rules = config.module.rules.concat(addConfig.module.rules);
+  // config.resolveLoader = addConfig.resolveLoader;
 };
 
 export default makeConfig({
