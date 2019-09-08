c4ab538800049c039e2f7b38973e47dd4307e03e
tkow <t.kusama0613@outlook.jp>
2019-09-09 02:09:37 +0900

    detox setting

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,17 +3,17 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  xit('should have welcome screen', async () => {
+  it('should have welcome screen', async () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
-  xit('should show hello screen after tap', async () => {
+  it('should show hello screen after tap', async () => {
     await element(by.id('hello_button')).tap();
     await expect(element(by.text('Hello!!!'))).toBeVisible();
   });
 
-  xit('should show world screen after tap', async () => {
+  it('should show world screen after tap', async () => {
     await element(by.id('world_button')).tap();
     await expect(element(by.text('World!!!'))).toBeVisible();
   });
-})
+});
